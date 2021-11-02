2b6870c8a44b50a61be843564fbe5c12c44f0670
vmermolenko <vmermolenko.dev@gmail.com>
2021-11-02 23:35:52 +0300

    const sequelize = new Sequelize(process.env.DATABASE_URL)

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: "http://localhost:3000/"  // here set your API url
+    baseURL: process.env.BASE_URL  // here set your API url
   },
   publicRuntimeConfig: {
     axios: {
