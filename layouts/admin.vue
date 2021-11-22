7117858ff6b547d9e416e2bc1cba661e8e7b76a9
vmermolenko <vmermolenko.dev@gmail.com>
2021-11-22 17:16:50 +0300

    width 100%

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -15,5 +15,8 @@
 </script>
 
 <style lang="scss" scoped>
-
+html,body, #__nuxt, #__layout{
+  height:100%!important;
+  width: 100%!important;
+}
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -38,5 +38,12 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+html,body, #__nuxt, #__layout{
+  height:100%!important;
+  width: 100%!important;
+}
+</style>
+
 
 
